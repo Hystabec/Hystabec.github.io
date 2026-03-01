8f1e8202cb63bec6d43ac55ae0f1eb43f9ee622b
Hystabec <edward14577777@gmail.com>
2026-03-01 01:57:10 +0000

    improved showcase page, minor wording changes
    The showcase selection can now be retained when when refreshing the page or can be linked to e.g. https://hystabec.github.io/showCase.html?ca=3 will link to Daedalus engine page.
    There is various hard coded values in the java script but is good for now

--- css_js/showCaseFunctionality.js
+++ css_js/showCaseFunctionality.js
@@ -1,22 +1,3 @@
-/*function resizeSideBar(defaultHeight, contentHeight)
-{
-    let sideBar = document.getElementById("sideBarBox");
-    let mainContent = document.getElementById("mainContentBox");
-
-    if(contentHeight > defaultHeight)
-    {
-        //sideBar.style.height = contentHeight + "px";
-        resizeTopBarExtra(widthTaken);
-        return contentHeight;
-    }
-    else
-    {
-        //sideBar.style.height = defaultHeight + "px";
-        resizeTopBarExtra(widthTaken);
-        return defaultHeight;
-    }
-}*/
-
 function resizeTopBarExtra(newSize)
 {
     let selecItemFiller = document.getElementById("selectionItem_Filler");
@@ -45,10 +26,51 @@ const selecContent3 = document.getElementById("selectionItemContent_3");
 const selecContent4 = document.getElementById("selectionItemContent_4");
 const asButtons = document.querySelectorAll(".selectionItem button");
 
+const maxSelIndex = 4;
+function selIndexToShort(index) {
+    switch(Number(index))
+    {
+    case 0:
+        return "DD";
+    case 1:
+        return "A";
+    case 2:
+        return "LH";
+    case 3:
+        return "DE";
+    case 4:
+        return "GE";
+    }
+    return "";
+}
+
 let currentlyActive = 0;
 
 //removing left border from the first element
 selecItem0.style.borderLeft = "none";
+
+//-------------Mobile/Small scale------------------
+const hamMen = document.querySelector(".hamburgerMenuToggleSub");
+const offScreenMen = document.querySelector(".hamburgerMenu");
+
+hamMen.addEventListener("click", () => {
+    hamMen.classList.toggle("active");
+    offScreenMen.classList.toggle("active");
+})
+
+function removeHamActive(){
+    hamMen.classList.remove("active");
+    offScreenMen.classList.remove("active");
+}
+
+const BM_selecItem0 = document.getElementById("BM_DD");
+const BM_selecItem1 = document.getElementById("BM_A");
+const BM_selecItem2 = document.getElementById("BM_LH");
+const BM_selecItem3 = document.getElementById("BM_DE");
+const BM_selecItem4 = document.getElementById("BM_FGE");
+
+const BM_asButtons = document.querySelectorAll(".BM_BUTTON");
+//-------------Mobile/Small scale END---------------
 
 function removeLastClasses(index)
 {
@@ -56,106 +78,121 @@ function removeLastClasses(index)
     {
     case 0:
         selecItem0.classList.remove("selected");
+        BM_selecItem0.classList.remove("active");
         selecContent0.classList.remove("active");
         break;
     case 1:
         selecItem1.classList.remove("selected");
+        BM_selecItem1.classList.remove("active");
         selecContent1.classList.remove("active");
         break;
     case 2:
         selecItem2.classList.remove("selected");
+        BM_selecItem2.classList.remove("active");
         selecContent2.classList.remove("active");
         break;
     case 3:
         selecItem3.classList.remove("selected");
+        BM_selecItem3.classList.remove("active");
         selecContent3.classList.remove("active");
         break;
     case 4:
         selecItem4.classList.remove("selected");
+        BM_selecItem4.classList.remove("active");
         selecContent4.classList.remove("active");
         break;
     }
 }
 
-asButtons[0].addEventListener("click", ()=>{
+let button0fun = ()=>{
     if(currentlyActive == 0)
         return;
 
     removeLastClasses(currentlyActive);
 
     selecItem0.classList.add("selected");
+    BM_selecItem0.classList.add("active");
     selecContent0.classList.add("active");
 
     currentlyActive=0;
-
-    //resizeSideBar(originalPageSize, selecContent0.getBoundingClientRect().height + selectionBarHeight);
+    setCAVal(0);
 
     switchViewCase("DD");
-});
-
-asButtons[1].addEventListener("click", ()=>{
+};
+let button1fun = ()=>{
     if(currentlyActive == 1)
         return;
 
     removeLastClasses(currentlyActive);
 
     selecItem1.classList.add("selected");
+    BM_selecItem1.classList.add("active");
     selecContent1.classList.add("active");
 
     currentlyActive=1;
-
-    //resizeSideBar(originalPageSize, selecContent1.getBoundingClientRect().height + selectionBarHeight);
+    setCAVal(1);
 
     switchViewCase("A");
-});
-
-asButtons[2].addEventListener("click", ()=>{
+};
+let button2fun = ()=>{
     if(currentlyActive == 2)
         return;
 
     removeLastClasses(currentlyActive);
 
     selecItem2.classList.add("selected");
+    BM_selecItem2.classList.add("active");
     selecContent2.classList.add("active");
 
     currentlyActive=2;
-
-    //resizeSideBar(originalPageSize, selecContent2.getBoundingClientRect().height + selectionBarHeight);
+    setCAVal(2);
 
     switchViewCase("LH");
-});
-
-asButtons[3].addEventListener("click", ()=>{
+};
+let button3fun = ()=>{
     if(currentlyActive == 3)
         return;
 
     removeLastClasses(currentlyActive);
 
     selecItem3.classList.add("selected");
+    BM_selecItem3.classList.add("active");
     selecContent3.classList.add("active");
 
     currentlyActive=3;
-
-    //resizeSideBar(originalPageSize, selecContent3.getBoundingClientRect().height + selectionBarHeight);
+    setCAVal(3);
 
     switchViewCase("DE");
-});
-
-asButtons[4].addEventListener("click", ()=>{
+};
+let button4fun = ()=>{
     if(currentlyActive == 4)
         return;
 
     removeLastClasses(currentlyActive);
 
     selecItem4.classList.add("selected");
+    BM_selecItem4.classList.add("active");
     selecContent4.classList.add("active");
 
     currentlyActive=4;
-
-    //resizeSideBar(originalPageSize, selecContent4.getBoundingClientRect().height + selectionBarHeight);
+    setCAVal(4);
 
     switchViewCase("GE");
-});
+};
+
+asButtons[0].addEventListener("click", button0fun);
+asButtons[1].addEventListener("click", button1fun);
+asButtons[2].addEventListener("click", button2fun);
+asButtons[3].addEventListener("click", button3fun);
+asButtons[4].addEventListener("click", button4fun);
+
+//-------------Mobile/Small scale---------------
+BM_asButtons[0].addEventListener("click", button0fun);
+BM_asButtons[1].addEventListener("click", button1fun);
+BM_asButtons[2].addEventListener("click", button2fun);
+BM_asButtons[3].addEventListener("click", button3fun);
+BM_asButtons[4].addEventListener("click", button4fun);
+//-------------Mobile/Small scale END---------------
 
 const originalPageSize = document.getElementById("sideBarBox").getBoundingClientRect().height;
 const selectionBarHeight = selecItem0.getBoundingClientRect().height;
@@ -224,6 +261,52 @@ function nextSlide(sh) {
     showSlide(gal_Index + 1, sh);
     resetInterval(sh);
 }
+//assignVariables("DD");
 
-assignVariables("DD");
-//-------------Gallery fun End-------------
+//-------------Selection saving-----------
+function setCAVal(newVal)
+{
+    const newURL = new URL(window.location);
+    newURL.searchParams.set("ca", newVal);
+    history.pushState(null, "", newURL);
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const caParam = urlParams.get("ca");
+if (caParam != null)
+{
+    currentlyActive = Number(caParam);
+    if(currentlyActive > maxSelIndex)
+        currentlyActive = 0;
+}
+
+switch(currentlyActive)
+{
+case 0:
+    selecItem0.classList.add("selected");
+    BM_selecItem0.classList.add("active");
+    selecContent0.classList.add("active");
+    break;
+case 1:
+    selecItem1.classList.add("selected");
+    BM_selecItem1.classList.add("active");
+    selecContent1.classList.add("active");
+    break;
+case 2:
+    selecItem2.classList.add("selected");
+    BM_selecItem2.classList.add("active");
+    selecContent2.classList.add("active");
+    break;
+case 3:
+    selecItem3.classList.add("selected");
+    BM_selecItem3.classList.add("active");
+    selecContent3.classList.add("active");
+    break;
+case 4:
+    selecItem4.classList.add("selected");
+    BM_selecItem4.classList.add("active");
+    selecContent4.classList.add("active");
+    break;
+}
+
+switchViewCase(selIndexToShort(currentlyActive));
